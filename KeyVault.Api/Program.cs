0e383f7a8fb4c9bee99fe5e2c2bac0f531f940c2
grahsp <pontus.grahsp@gmail.com>
2026-04-06 21:20:11 +0200

    feat(api): configure auth pipeline and expose me endpoint

--- KeyVault.Api/Program.cs
+++ KeyVault.Api/Program.cs
@@ -1,8 +1,9 @@
+using KeyVault.Api.Authentication;
 using KeyVault.Api.DependencyInjection;
-using KeyVault.Application;
 using KeyVault.Application.Authentication;
 using KeyVault.Application.DependencyInjection;
 using KeyVault.Infrastructure.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
 
 namespace KeyVault.Api;
 
@@ -12,14 +13,33 @@ public class Program
 	{
 		var builder = WebApplication.CreateBuilder(args);
 
+		builder.Configuration
+			.AddJsonFile("appsettings.json")
+			.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true)
+			.AddUserSecrets<Program>(true)
+			.AddEnvironmentVariables();
+		
 		if (builder.Environment.IsDevelopment())
 		{
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen(options =>
-			{
-				options.OperationFilter<DevSubjectHeaderFilter>();
-			});
+				options.OperationFilter<DevSubjectHeaderFilter>());
+			
+			builder.Services.AddAuthentication("Dev")
+				.AddScheme<AuthenticationSchemeOptions, DevAuthenticationHandler>("Dev", null);
 		}
+
+		if (builder.Environment.IsProduction())
+		{
+			builder.Services.AddAuthentication("Bearer")
+				.AddJwtBearer("Bearer", options =>
+				{
+					options.Authority = "https://dev-80amfbvnreq8wgr1.us.auth0.com/";
+					options.Audience = "https://keyvault.com";
+				});
+		}
+		
+		builder.Services.AddAuthorization();
 		
 		builder.Services.AddApiServices();
 		builder.Services.AddApplicationServices();
@@ -34,9 +54,16 @@ public class Program
 		}
 
 		app.UseAuthentication();
+		app.UseMiddleware<CurrentUserMiddleware>();
 		app.UseAuthorization();
 
 		app.UseHttpsRedirection();
+
+		app.MapGet("/me", (ICurrentUser user) => Results.Ok(new
+		{
+			Id = user.UserId,
+			Status = user.Status
+		}));
 
 		app.Run();
 	}
