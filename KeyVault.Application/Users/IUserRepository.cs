2f46d581686e3927f70c4d5ed1d401c928aaf6b8
grahsp <pontus.grahsp@gmail.com>
2026-04-08 12:05:18 +0200

    feat: add method to fetch user aggregate in user repository

--- KeyVault.Application/Users/IUserRepository.cs
+++ KeyVault.Application/Users/IUserRepository.cs
@@ -4,6 +4,7 @@ namespace KeyVault.Application.Users;
 
 public interface IUserRepository
 {
+	Task<User?> GetByIdAsync(Guid id, CancellationToken ct);
 	void Add(User user);
 	void Remove(User user);
 }
--- KeyVault.Infrastructure/Persistence/Repositories/EfUserRepository.cs
+++ KeyVault.Infrastructure/Persistence/Repositories/EfUserRepository.cs
@@ -1,10 +1,14 @@
 using KeyVault.Application.Users;
 using KeyVault.Domain.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace KeyVault.Infrastructure.Persistence.Repositories;
 
 public class EfUserRepository(AppDbContext context) : IUserRepository
 {
+	public Task<User?> GetByIdAsync(Guid id, CancellationToken ct) =>
+		context.Users.SingleOrDefaultAsync(x => x.Id == id, ct);
+	
 	public void Add(User user)
 	{
 		context.Users.Add(user);
