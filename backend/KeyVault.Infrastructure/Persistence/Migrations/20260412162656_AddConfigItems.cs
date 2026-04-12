79a4a6efed86db75477de570ecff331e99cdc845
grahsp <pontus.grahsp@gmail.com>
2026-04-12 18:37:11 +0200

    chore: add new migration for adding config items table

---
+++ backend/KeyVault.Infrastructure/Persistence/Migrations/20260412162656_AddConfigItems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KeyVault.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConfigItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "config_items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Key = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_config_items", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_config_items_ProjectId_Key",
+                table: "config_items",
+                columns: new[] { "ProjectId", "Key" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "config_items");
+        }
+    }
+}
