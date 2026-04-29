fe03209e80c8e09236d2cc312c4d25212afc4872
grahsp <pontus.grahsp@gmail.com>
2026-04-29 09:14:16 +0200

    feat: remove uneccessary machine claims to essentials

--- backend/KeyVault.Infrastructure/Authentication/ScopeCapabilities.cs
+++ backend/KeyVault.Infrastructure/Authentication/ScopeCapabilities.cs
@@ -1,6 +1,5 @@
 using KeyVault.Application.Abstractions.Identity;
 using KeyVault.Application.Authorization.Capabilities;
-using KeyVault.Domain.Projects;
 
 namespace KeyVault.Infrastructure.Authentication;
 
@@ -12,23 +11,8 @@ public class ScopeCapabilities : IScopeCapabilities
 		{
 			switch (scope)
 			{
-				case "config:read":
+				case "secret:read":
 					yield return ProjectCapability.Create(ProjectResource.ConfigValue, ProjectPermission.Read);
-					break;
-				case "config:write":
-					yield return ProjectCapability.Create(ProjectResource.ConfigValue, ProjectPermission.Write);
-					break;
-				case "config:manage":
-					yield return ProjectCapability.Create(ProjectResource.ConfigItem, ProjectPermission.Manage);
-					break;
-				case "project:members:manage":
-					yield return ProjectCapability.Create(ProjectResource.ProjectMember, ProjectPermission.Manage);
-					break;
-				case "project:environments:manage":
-					yield return ProjectCapability.Create(ProjectResource.Environment, ProjectPermission.Manage);
-					break;
-				case "project:delete":
-					yield return ProjectCapability.Create(ProjectResource.Project, ProjectPermission.Delete);
 					break;
 			}
 		}
