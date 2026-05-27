047794c85c2b90be3947cd28b40ab28e17cb62db
grahsp <pontus.grahsp@gmail.com>
2026-05-27 10:57:41 +0200

    chore: add matchMedia test shim

--- frontend/src/test/setupTests.ts
+++ frontend/src/test/setupTests.ts
@@ -1,1 +1,14 @@
 import '@testing-library/jest-dom/vitest'
+
+if (!window.matchMedia) {
+  window.matchMedia = (query: string): MediaQueryList => ({
+    addEventListener: () => undefined,
+    addListener: () => undefined,
+    dispatchEvent: () => false,
+    matches: false,
+    media: query,
+    onchange: null,
+    removeEventListener: () => undefined,
+    removeListener: () => undefined,
+  })
+}
