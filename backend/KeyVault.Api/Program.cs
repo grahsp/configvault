50e1a95b07862c990b6b357e8af7adff39b45532
grahsp <pontus.grahsp@gmail.com>
2026-06-08 12:31:25 +0200

    fix: update obsolete format of appsettings for test environment - remove redundant logic in Program.cs for loading variables

--- backend/KeyVault.Api/Program.cs
+++ backend/KeyVault.Api/Program.cs
@@ -18,12 +18,6 @@ public class Program
 	public static void Main(string[] args)
 	{
 		var builder = WebApplication.CreateBuilder(args);
-
-		builder.Configuration
-			.AddJsonFile("appsettings.json")
-			.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true)
-			.AddUserSecrets<Program>(true)
-			.AddEnvironmentVariables();
 		
 		builder.Services.AddApiModule(builder.Configuration, builder.Environment);
 		builder.Services.AddAuthenticationModule(builder.Configuration, builder.Environment);
